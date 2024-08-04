aa377711834e8d0d000e8162c2b7046b6500749f
HarryCode15 <harshbtekade137@gmail.com>
2024-08-05 01:57:18 +0530

    adding props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,21 @@
 import kaite from "../images/person.png";
 import star from "../images/Star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <>
-      <div className='card'>
-        <img src={kaite} alt="Person pic" className='card-image'/>
+      <div className="card">
+        <img src={kaite} alt="Person pic" className="card-image" />
         <div className="card-stat">
-            <img src={star} alt="Star" className="card-star"/>
-            <span>5.0</span>
-            <span className='gray'>(6) •</span>
-            <span className='gray'>USA</span>
+          <img src={star} alt="Star" className="card-star" />
+          <span>5.0</span>
+          <span className="gray">(6) •</span>
+          <span className="gray">USA</span>
         </div>
-        <p>Life Lessons with Kaite</p>
-        <p><span className='cost'>From $136</span> / person</p>
+        <h2>Life Lessons with Kaite</h2>
+        <p>
+          <span className="cost">From $136</span> / person
+        </p>
       </div>
     </>
   );
